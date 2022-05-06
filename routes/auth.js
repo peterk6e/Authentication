28f1c3a17e259affa1855b429b985e192762d20c
pierrek6e <pierrecass75@gmail.com>
2022-05-06 09:50:03 +0700

    login route

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,18 @@ const rounds = 10;
 const jwt = require("jsonwebtoken");
 const tokenSecret = "my-token-secret";
 
-router.get("/login", (req, res) => {});
+router.get("/login", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (!user) res.status(404).json({ error: "no user with that email found" });
+    else {
+      bcrypt.compare(req.body.password, user.password, (error, match) => {
+        if (error) res.status(500).json(error);
+        else if (match) res.status(200).json({ token: generateToken(user) });
+        else res.status(403).json({ error: "passwords do not match" });
+      });
+    }
+  });
+});
 
 router.post("/signup", (req, res) => {
   bcrypt.hash(req.body.password, rounds, (error, hash) => {

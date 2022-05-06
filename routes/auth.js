c4fbd8aee5bec9883cda7159a0b8468b3ed64142
pierrek6e <pierrecass75@gmail.com>
2022-05-06 09:14:27 +0700

    signup route

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const User = require("../models/user");
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {});
 
-});
-
-router.post('/signup', (req, res) => {
-
+router.post("/signup", (req, res) => {
+  bcrypt.hash(req.body.password, rounds, (error, hash) => {
+    if (error) res.status(500).json(error);
+    else {
+      const NewUser = User({ email: req.body.email, password: hash });
+      NewUser.save()
+        .then((user) => {
+          res.status(200).json(user);
+        })
+        .catch((error) => {
+          error.status(500).json(error);
+        });
+    }
+  });
 });
 
 module.exports = router;
